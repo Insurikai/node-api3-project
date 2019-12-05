c49dc22039291ccccf5fcb60fdf76c42d0959e2e
Michael Jordan <Insurikai@gmail.com>
2019-12-05 14:26:29 -0800

    finished

--- posts/postRouter.js
+++ posts/postRouter.js
@@ -1,21 +1,49 @@
 const express = require('express');
-
+const db = require('./postDb');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  // do your magic!
+  db.get()
+    .then(posts => {
+      res.status(200).send(posts);
+    })
+    .catch(err => {
+      res.status(500).send({ error: err });
+    });
 });
 
 router.get('/:id', (req, res) => {
-  // do your magic!
+  db.getById(req.params.id)
+    .then(post => {
+      post
+        ? res.status(200).send(post)
+        : res.status(404).send(`No post with id ${req.params.id}.`);
+    })
+    .catch(err => {
+      res.status(500).send({ error: err });
+    });
 });
 
 router.delete('/:id', (req, res) => {
-  // do your magic!
+  db.remove(req.params.id)
+    .then(recordsDeleted => {
+      recordsDeleted > 0
+        ? res.status(200).send(`Successfully deleted post with id ${req.params.id}.`)
+        : res.status(404).send(`No post with id ${req.params.id}.`);
+    })
+    .catch(err => {
+      res.status(500).send({ error: err.toString() });
+    });
 });
 
 router.put('/:id', (req, res) => {
-  // do your magic!
+  db.update(req.params.id, req.body)
+    .then(changes => {
+      res.status(200).send(`Made ${changes} changes.`)
+    })
+    .catch(err => {
+      res.status(500).send({ error: err.toString() });
+    });
 });
 
 // custom middleware
--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 const express = require('express');
-
 const server = express();
-
-server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
-});
+const userRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter')
 
 //custom middleware
+server.use(express.json())
+function logger(req, res, next) {
+  console.log(
+    `${req.method} '${req.protocol}://${req.hostname}${req.path}' ${new Date()}`,
+    `Params ${req.params.toString()}.`,
+    `Body ${req.body}.`
+    );
 
-function logger(req, res, next) {}
+  next();
+}
+server.use(logger)
+
+server.get('/', (req, res) => {
+  
+  res.send(`<h2>Let's write some middleware!</h2>`);
+});
+server.use('/api/users',userRouter);
+server.use('/api/posts',postsRouter);
 
 module.exports = server;
